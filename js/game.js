55c285b9c3f1758d0872b641c33bf2d7f93c22b5
Daniel Cornejo <contact@danielcornejo.dev>
2023-04-15 15:24:43 -0400

    Iteration 1 complete

--- js/game.js
+++ js/game.js
@@ -1,11 +1,51 @@
 class Game {
-    constructor(){}
+    constructor(){
+        this.startScreen = document.getElementById("game-intro");
+        this.gameScreen = document.getElementById("game-screen");
+        this.gameEndScreen = document.getElementById("game-end");
+        this.gameScore = document.getElementById("score");
+        this.gameLives = document.getElementById("lives");
+        this.player = null;
+        this.height = 600;
+        this.width = 500;
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+    }
 
-    start() {}
+    start() {
+        //sets initial width and heigh of the game-screen when start method is called.
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
 
-    gameLoop() {}
+        // hide start screen
+        this.startScreen.style.display = "none";
+        // show game screen
+        this.gameScreen.style.display = "block";
+        
+        //starts the gameLoop method
+        this.gameLoop();
+    }
 
-    update() {}
+    gameLoop() {
+
+        console.log("in the game loop");
+
+        // if gameIsOver is true, interrupts the function to stop the loop.
+        if (this.gameIsOver){
+            return;
+        }
+
+        this.update();
+
+        //recursive loop to invoke itself repeatedly. Ensures a consistest frame rate 
+        window.requestAnimationFrame()() => this.gameLoop();
+    }
+
+    update() {
+        console.log("in the update");
+    }
 
     endGame() {}
 }
